f93009aaad46e55be5adf119f6763b85822b8aee
wongsunlung <wongsunlung@gmail.com>
2026-05-12 10:36:15 +0800

    step 2 : create pages app

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
-def index(req):
-    return HttpResponse("<h1>Hello, world ! </h1>")
+# def index(req):
+#     return HttpResponse("<h1>Hello, world ! </h1>")
+
+def index(request):
+    return render(request, "pages/index.html")
+def about(request):
+    return render(request, "pages/about.html")
