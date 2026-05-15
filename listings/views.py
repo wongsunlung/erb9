feb61639f8b97d83758a1f5e97155ef07cd3ad85
wongsunlung <wongsunlung@gmail.com>
2026-05-15 19:34:03 +0800

    step 7 : create listing, doctor

--- listings/views.py
+++ listings/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from .models import Listing
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 def listings(request):#打少t
     listings = Listing.objects.all()
-    context = {"listings": listings}
-    print("data : ", [listings] ,"--end  --")
+    paginator = Paginator(listings, 3)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+    context = {"listings": paged_listings}
     return render(request, "listings/listings.html", context)
 
 
