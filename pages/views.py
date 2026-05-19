859c75033604e0c6c533be43362783135d909110
wongsunlung <wongsunlung@gmail.com>
2026-05-19 15:11:22 +0800

    step 10 about.html done

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from listings.models import Listing
 from listings.choices import district_choices, room_type_choices, rooms_choices
+from doctors.models import Doctor
 # from django.http import HttpResponse
 
 # Create your views here.
@@ -12,4 +13,8 @@ def index(request):
     context = {"listings":listings, "district_choices": district_choices, "room_type_choices": room_type_choices, "rooms_choices": rooms_choices}
     return render(request, "pages/index.html", context)
 def about(request):
-    return render(request, "pages/about.html")
+    doctors = Doctor.objects.order_by('-hire_date')[:3]
+    mvp_doctors = Doctor.objects.all().filter(is_mvp=True)
+    context = {"doctors":doctors, "mvp_doctors":mvp_doctors }
+    print(mvp_doctors)
+    return render(request, "pages/about.html", context)
